c36132ca3cc05ee07d564bacb980657675619dbf
kunal052001 <kunalsadashivpatil@gmail.com>
2025-02-11 22:46:37 +0530

    day 6 of practice

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import Api from './componets/Api'
+import Mode from './componets/Mode'
+// import Api from './componets/Api'
+// import Paggination from './componets/Paggination'
 // import Button from './componets/Button.js'
 
 export default function kunal() {
   return (
     
+
     <>
-    
+     <h1> hay this is kunal</h1>
       {/* <Button/> */}
-      <Api></Api>
+     <Mode/>
+      {/* <Api></Api> */}
+      {/* <Paggination/> */}
     </>
    
   )
--- myapp/src/componets/Api.js
+++ myapp/src/componets/Api.js
@@ -3,6 +3,8 @@ import React, { useEffect,useState} from "react";
 
 function Api() {
   const [state, setState] = useState([]);
+  const [page,setPage]=useState(1)
+  const no=3;
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
@@ -15,15 +17,32 @@ function Api() {
       .catch((err) => console.error("Error fetching data:", err));
   }, []);
 
+    const lastindex=page*no;
+    const firstindex=lastindex-no;
+    const cardas=state.slice(firstindex,lastindex)
+    const totalpage=Math.ceil(state.length/no);
 const styles={
   field:{
     width: '100wh',
-    height: '100vh'
+    height: '100vh',
+    justifyContent:'centre',
+   alignItem:'centre',
+   display:'flex',
+   flexDirection:'column',
+   paddingLeft:'300px'
+    
+  //  display: "flex",
+  //  flexDirection: "column",
+  //  alignItems: "center",
+  //  justifyContent: "center",
+  //  minHeight: "100vh",
+  //  padding: "20px",
   },
   area:{
     display:'flex',
     flexWrap:'wrap',
-    gap:'10px'
+    gap:'10px',
+    justifyContent:'centre'
   },
   card:{
       border:'2px solid black',
@@ -44,7 +63,7 @@ const styles={
     
           <div style={styles.area}>
         {
-          state.length > 0 ?(state.map((arr)=>(
+          cardas.length > 0 ?(cardas.map((arr)=>(
             <div style={styles.card} key={arr.id}>
                 <h1>{arr.title}</h1>
                 <p>{arr.description}</p>
@@ -54,6 +73,11 @@ const styles={
 
           ) :(<div>..........loading</div>)
         }
+      </div>
+      <div>
+        <button onClick={()=>setPage(page-1)} >PREV</button>
+        <span style={{backgroundColor:'red',font:'bold'}}>{page}outof{totalpage}</span>
+        <button onClick={()=>setPage(page+1)}> NEXT</button>
       </div>
     </div>
   );
